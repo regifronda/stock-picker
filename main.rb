74e8bfe3203bd6e1eeff25fdc161a6fe97858109
Regi Fronda <regisfronda@gmail.com>
2021-06-15 03:47:01 -0700

    Create stock_picker method

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 # create a method with one argument
+def stock_picker(stock_prices)
+end
 # the argument is an array of stock prices, one for each day
 # iterate through the array. 
 # each iteration, look for the biggest difference between a latter stock price and a former stock price.
 # take the index of those two days into an array
 # return the array of two days
 
-
+stock_picker([17,3,6,9,15,8,6,1,10])
