cdd6222131b536b4201eaae66a6f08ba38e18785
Regi Fronda <regisfronda@gmail.com>
2021-06-15 23:58:24 -0700

    Fix each_with_index method with buy_day

--- main.rb
+++ main.rb
@@ -5,7 +5,9 @@ def stock_picker(stock_prices)
 
   binding.pry
   
-  stock_prices.each_with_index do |starting_price, buy day
+  stock_prices.each_with_index do |buy_price, buy_day|
+
+  end
   # the argument is an array of stock prices, one for each day
   # iterate through the array. 
   # each iteration, look for the biggest difference between a latter stock price and a former stock price.
