7e42eb34cf28b5116ccead1f469b75ad1e1a64b9
Regi Fronda <regisfronda@gmail.com>
2021-06-16 01:42:33 -0700

    Assign best days to sell and buy to array best_days

--- main.rb
+++ main.rb
@@ -1,28 +1,25 @@
-require 'pry'
 def stock_picker(stock_prices)
   best_days = Array.new(2)
   best_profit = 0
 
-  binding.pry
   
   stock_prices.each_with_index do |buy_price, buy_day|
     stock_prices.each_with_index do |sell_price, sell_day|
       next if sell_day <= buy_day
       if sell_price - buy_price >= best_profit
         best_profit = sell_price - buy_price
+        best_days[0] = buy_day
+        best_days[1] = sell_day
         
+
       end
-      
     end
   end
-  # the argument is an array of stock prices, one for each day
-  # iterate through the array. 
-  # each iteration, look for the biggest difference between a latter stock price and a former stock price.
-  # take the index of those two days into an array
   # return the array of two days
-  #end
+  best_days
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
 
-binding.pry
+p stock_picker([17,3,6,9,15,8,6,1,10])
+
