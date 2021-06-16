691753ea6a23f73665b1fc49e63991fec9db6b0d
Regi Fronda <regisfronda@gmail.com>
2021-06-15 23:36:18 -0700

    Create variable best_profit

--- main.rb
+++ main.rb
@@ -2,9 +2,17 @@ require 'pry'
 # create a method with one argument
 def stock_picker(stock_prices)
   best_days = []
+  best_profit = 0
   
   binding.pry
+  
+  for i in stock_prices do
+    subtraction = i - (i[+1])
+    binding.pry
+  end
+  #stock_prices.each_with_index do |stock_price, day|
 
+  #end
 end
 # the argument is an array of stock prices, one for each day
 # iterate through the array. 
