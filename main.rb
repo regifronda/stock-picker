3f6c96f3d9babc7b61f017f79ea3e69ba6f649c7
Regi Fronda <regisfronda@gmail.com>
2021-06-16 01:47:41 -0700

    Remove comments and Pry-byebug lines

--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@ def stock_picker(stock_prices)
   best_days = Array.new(2)
   best_profit = 0
 
-  
   stock_prices.each_with_index do |buy_price, buy_day|
     stock_prices.each_with_index do |sell_price, sell_day|
       next if sell_day <= buy_day
@@ -10,16 +9,10 @@ def stock_picker(stock_prices)
         best_profit = sell_price - buy_price
         best_days[0] = buy_day
         best_days[1] = sell_day
-        
-
       end
     end
   end
-  # return the array of two days
   best_days
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
-
-p stock_picker([17,3,6,9,15,8,6,1,10])
-
