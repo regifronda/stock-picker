41d2cafbd95a09fd9502288a9536dedfffd290e5
Regi Fronda <regisfronda@gmail.com>
2021-06-15 23:54:37 -0700

    Organize comments

--- main.rb
+++ main.rb
@@ -1,24 +1,18 @@
 require 'pry'
-# create a method with one argument
 def stock_picker(stock_prices)
   best_days = Array.new(2)
   best_profit = 0
 
   binding.pry
   
-  for i in stock_prices do
-    subtraction = i - (i[+1])
-    binding.pry
-  end
-  #stock_prices.each_with_index do |stock_price, day|
-
+  stock_prices.each_with_index do |starting_price, buy day
+  # the argument is an array of stock prices, one for each day
+  # iterate through the array. 
+  # each iteration, look for the biggest difference between a latter stock price and a former stock price.
+  # take the index of those two days into an array
+  # return the array of two days
   #end
 end
-# the argument is an array of stock prices, one for each day
-# iterate through the array. 
-# each iteration, look for the biggest difference between a latter stock price and a former stock price.
-# take the index of those two days into an array
-# return the array of two days
 
 stock_picker([17,3,6,9,15,8,6,1,10])
 
